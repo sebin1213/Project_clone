16c29b98d93d1aaa7ac4a3507235213659c68f7e
inah <kimih1200@gmail.com>
2021-01-31 16:59:56 +0900

    ready_content-hover시 나타나는 css 및 js 적용

--- inflearn/js/ready_lecture.js
+++ inflearn/js/ready_lecture.js
@@ -15,8 +15,31 @@ $(function(){
         function(){ // 마우스 내렸을 때
             $('#'+index+'>.hoverBox').css('display','none');
         }
-    ); // hover close
+    ); // content hover close
 
-
+    /************************ ICON Hover ************************/
+    $('.hoverMenu img').hover(
+        function(){
+            icon_class=$(this).attr('class');
+            if(icon_class=='addCart'){
+                $('.'+icon_class).attr('src','image/cart02.png')
+                $('.'+icon_class).before( '<p>바구니에 추가하기</p>' )
+            }else if(icon_class=='addHeart'){
+                $('.'+icon_class).attr('src','image/heart02.png')
+                $('.'+icon_class).before( '<p>위시리스트에 추가하기</p>' )
+            }else{
+                $('.'+icon_class).before( '<p>내 목록에 추가</p>' )
+            }
+           $('.hoverMenu>div>p').addClass('arrow_box');
+        },
+        function(){
+            if(icon_class=='addCart'){
+                $('.'+icon_class).attr('src','image/cart.png')
+            }else if(icon_class=='addHeart'){
+                $('.'+icon_class).attr('src','image/heart.png')
+            }
+            $('.hoverMenu>div>p').remove(); // hover 시 생성된 <p> 제거
+        }
+    ); // icon hover close
 
 }); // function() close
