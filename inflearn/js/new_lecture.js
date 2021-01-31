c205f5e793158c9fb8d7376eea0906aedf70bfc8
sebin1213 <1213hope@gmail.com>
2021-02-01 00:05:03 +0900

    풀받을려고 커밋..

--- inflearn/js/new_lecture.js
+++ inflearn/js/new_lecture.js
@@ -18,46 +18,102 @@
 
 }); // function() 종료 **/
 
+
+
+/* slideShow.js */
+
+
 $(function(){
-    // 이동한 이미지의 index 값 저장 (현재 보고 있는 이미지-현재위치)
+    //이동한 이미지의 index값 저장(현재 보고있는 이미지- 현재위치)
     var movedIndex=0;
+    var length=$('#new_lecture_content>div').length; // 나열된 컨텐츠 개수 10개
 
-    // 슬라이드 패널을 움직여주는 함수
+    // 슬라이드 패널을 움직여주는 함수 - 사용자정의
     function moveSlide(index){
-        // 전달받은 index 값을 movedIndex 에 저장
-        movedIndex = index; // 전역변수
-
-        var boxWidth= $('#new_lecture_slide').width();
-        // 슬라이드 이동
+        //전달 받은 index값을 moveIndex에 저장
+        movedIndex = index;
+        var boxWidth= $('#new_lecture_lecture').width();
         var moveLeft= -(index*boxWidth);
-        $('#new_lecture_list').animate({left:moveLeft},'slow');
-
-    }
-
-    var length=$('.slideImage').length;
-    // prevButton 클릭시 앞으로 이동
-    $('#new_lecture_prevBtn').on('click',function(){
-        // 버튼 클릭시 보여줘야 할 이미지 index 결정해서
-        movedIndex -= 1; // 현재 이미지 이전 이미지의 인덱스로 설정
-
-        if(movedIndex<0)
-            movedIndex=length-1;
+        $('#new_lecture_content').animate({left:moveLeft},'slow');
+    };
 
 
-        // moveSlide() 호출
-        moveSlide(movedIndex);
-    }); // prevBtn close
+    //prev 버튼 클릭하면 앞으로 이동
+    $('#prev_new_lecture_Btn').on('click',function(){
+        moveIndex = moveIndex - 1; //현재 이미지 이전이미지의 인덱스로 바꿈
+       //moveSlide() 호출
+       moveSlide(moveIndex);
+    });
+    $('#next_new_lecture_Btn').on('click',function(){
+        moveIndex = moveIndex + 1; //현재 이미지 이전이미지의 인덱스로 바꿈
+       //moveSlide() 호출
 
-    // nextButton 클릭시 뒤로 이동
-    $('#new_lecture_nextBtn').on('click',function(){
-        // 버튼 클릭시 보여줘야 할 이미지 index 결정해서
-        movedIndex += 1; // 다음 이미지의 인덱스
+       moveSlide(moveIndex);
+    }); //next버튼 종료
 
-        if(movedIndex>=length)
-            movedIndex=0;
+})
 
-        // moveSlide() 호출
-        moveSlide(movedIndex);
-    }); // nextBtn close
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ready_lecture.js
+*/
+
+$(function(){
+
+    /*** Click Event ***/
+    var movedPage=0;
+    var length=$('#ready_content>div').length; // 나열된 컨텐츠 개수 8개
+
+    function moveSlide(index){
+        movedPage = index;
+
+        var boxWidth= $('#ready_lecture').width();
+        var moveLeft= -(index*boxWidth);
+        $('#ready_content').animate({left:moveLeft},'slow');
+    } // moveSlide function() close
+
+    $('#prevBtn02').on('click',function(){
+        if(movedPage != 0){
+            movedPage--;
+            $(this).prop("disabled", false);
+        }
+        else
+            (this).prop("disabled", true);
+        moveSlide(movedPage);
+    }); // prevBtn click close
+
+    $('#nextBtn02').on('click',function(){
+        if(movedPage != length/4-1){
+            movedPage++;
+            $(this).prop("disabled", false);
+        }else
+            $(this).prop("disabled", true);
+        moveSlide(movedPage);
+    }); // nextBtn click close
+
+
+}); // function() close
+
+
