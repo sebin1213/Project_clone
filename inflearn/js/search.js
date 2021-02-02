60bc00b71a53e7d37bcb1e60f8d634a63459d93d
sebin1213 <1213hope@gmail.com>
2021-02-02 14:23:34 +0900

    search js 추가 - 클릭시 이동

--- inflearn/js/search.js
+++ inflearn/js/search.js
@@ -13,4 +13,37 @@ $(function(){
             }
         );
     });
+
+/********************************* search 커서 변경 **************************************/
+
+     $('#search').each(function(index){
+        $(this).hover(
+            function(){
+                $('#search').css('cursor' , 'text');
+            },
+            function(){
+                $('#search').css('cursor' , 'pointer');
+            }
+        );
+    });
+
+
+/********************************* search 클릭시 이동 **************************************/
+
+    $('#search').on('click',function(){
+        if($(document).scrollTop()>=$('#headerBox').height()){
+            var location = document.querySelector("#search_main").offsetTop;
+            var menuHeight = document.querySelector("#headerBox").offsetHeight;
+            s_location= location -menuHeight;
+            window.scrollTo({top:s_location, behavior:'smooth'});
+
+            }
+        else{
+            var location = document.querySelector("#search_main").offsetTop;
+            var menuHeight = document.querySelector("#headerBox").offsetHeight;
+            s_location= location -menuHeight-70;
+            window.scrollTo({top:s_location, behavior:'smooth'});
+            }
+    });
+
 })
