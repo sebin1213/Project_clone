9a13588a631ebc14b42e1f3995927d45aa1a7442
sebin1213 <73850629+sebin1213@users.noreply.github.com>
2021-11-02 00:47:06 +0900

    Update urls.py

--- inflearn/combot/urls.py
+++ inflearn/combot/urls.py
@@ -10,4 +10,5 @@ urlpatterns =[
     path('<int:question_id>/', views.community_post, name='community_post'),
     path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'), #답변을 등록하는 url
     path('question/create/', views.question_create,name='question_create'),
+    path('question/delete/<int:question_id>/', views.question_delete,name='question_delete'),
 ]
